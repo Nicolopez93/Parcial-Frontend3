4624f19e2c81d50efd2a6c1e068bc45d8954c9db
Nicolopez93 <nicolopezarq@gmail.com>
2023-11-02 19:35:38 -0300

    Examen Parcial

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Card.css'
 
-const Card = () => {
+
+const Card = ({nombre, apellido, edad}) => {
   return (
-    <div>
-      Hola soy una tarjeta
+  <>
+    <div className='Card'>
+      <h2>El Alumno {apellido}, {nombre} de {edad}  años 
+      se registro correctamente</h2>
     </div>
+  </>
   )
 }
 
